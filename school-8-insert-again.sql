ada9a47f7cd112e44c115a1cf60c8f9225c65e45
Kajalkaran Shah <drkajal.sh@gmail.com>
2025-01-16 22:44:40 +0000

    school8

---
+++ school-8-insert-again.sql
@@ -0,0 +1,20 @@
+INSERT INTO students(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    "Jane",
+    "Doe",
+    "janedoe@gmail.com"
+);
+INSERT INTO students(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    "Chris",
+    "Dior",
+    "chrisdior@example.com"
+);
